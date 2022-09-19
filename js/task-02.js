e3dfd7b06e93212411d48cdf6f5f8e43e027f571
MargoTrigub <margotrigub@gmail.com>
2022-09-19 15:30:40 +0300

    Initial commit

---
+++ js/task-02.js
@@ -0,0 +1,17 @@
+const ul = document.querySelector("ul");
+const ingredients = [
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
+];
+const createElementLi = (name) => {
+  let li = document.createElement("li");
+  li.setAttribute("class", "item");
+  li.textContent = name;
+  return li;
+};
+let element = ingredients.map((element) => createElementLi(element));
+ul.append(...element);
---
+++ js/task-04.js
@@ -0,0 +1,13 @@
+const incr = document.querySelector('button[data-action="increment"]');
+const decr = document.querySelector('button[data-action="decrement"]');
+const value = document.querySelector("#value");
+let currentNumber = 0;
+
+incr.addEventListener("click", (event) => {
+  currentNumber++;
+  value.textContent = currentNumber;
+});
+decr.addEventListener("click", (event) => {
+  currentNumber--;
+  value.textContent = currentNumber;
+});
